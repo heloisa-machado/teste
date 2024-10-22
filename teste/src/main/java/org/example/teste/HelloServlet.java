8527c5be642e2461016a56a9ab614f770386d5f1
heloisa-machado <heloisa.machado@germinare.org.br>
2024-10-22 12:42:19 -0300

    teste env

--- teste/src/main/java/org/example/teste/HelloServlet.java
+++ teste/src/main/java/org/example/teste/HelloServlet.java
@@ -20,6 +20,7 @@ public class HelloServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+        out.println("<h1>"+"teste"+"</h1>");
         out.println("</body></html>");
     }
 
